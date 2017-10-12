4c7e3f0fbff3bb74d40b419534ca86906bb81a9d
Greg56 <32373129+Greg56@users.noreply.github.com>
2017-10-12 15:44:41 -0400

    Update Maximum.h

--- Lab1/Maximum.h
+++ Lab1/Maximum.h
@@ -7,7 +7,22 @@ namespace labI
 {
 	float Maximum(float data[], int size)
 	{
-		return -1;
+	   data.dat()
+	   for(i = 0; i < n; ++i)
+	{
+		cout << "Enter Number " << i + 1 << " : ";
+		cin >> data[i];
+	}
+
+		    // Loop to store largest number to arr[0]
+		for(i = 1;i < n; ++i)
+		    {
+		       // Change < to > if you want to find the smallest element
+			if(data[0] < data[i])
+			   data[0] = data[i];
+		    }
+		    cout << "Largest element = " << data[0];
+			return -1;
 	}
 }
 

38349fa11d01e5c808ea5c97795562ee14c8e2e8
Greg56 <32373129+Greg56@users.noreply.github.com>
2017-10-12 07:58:38 -0700

    Update Mean.h

--- Lab1/Mean.h
+++ Lab1/Mean.h
@@ -7,6 +7,12 @@ namespace labI
 {
 	float Mean(float data[], int size)
 	{
+	 for(int x = 0; x< a.size; x++
+	     {
+		total += a[x];
+		}
+		mean = total/a.size;
+		cout << mean << "is the average" << endl; 
 		return -1;
 	}
 }

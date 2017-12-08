3733609fa90ab13f65df61dfb7a478e0a7ba7d79
Greg56 <32373129+Greg56@users.noreply.github.com>
2017-12-08 09:57:43 -0500

    Update Name.h

--- LAB3/Name.h
+++ LAB3/Name.h
@@ -2,31 +2,171 @@
 //Author: Gregory Ayo
 //Creation: 12/7/17
 #ifndef NAME_H
+
 #define NAME_H
 
 #include "Archive.h"
 
 namespace lab3
+
 {
-	class Name
-	{
-		private:
-		std::string firstName;
-		std::string lastName;
-		
-		public:
-		Name();
-		Name(std::string,std::string);
-		Name(const Name&);
-		Name& operator=(const Name&);
-		~Name() {}
-		std::string& GetFirstName();
-		std::string& GetLastName();
-		void SetFirstName(const std::string&);
-		void SetLastName(const std::string&);
-		std::string ToString() const;
-		friend std::ostream& operator<<(std::ostream&,const Name&);
-	};
+
+class Name
+
+{
+
+private:
+
+std::string firstName;
+
+std::string lastName;
+
+public:
+
+Name()
+
+{
+
+firstName = "";
+
+lastName = "";
+
 }
 
-#endif 
+Name(std::string firstName,std::string lastName)
+
+{
+
+for(int i = 0;i < firstName.length();i+=1)
+
+{
+
+if(!isalpha(firstName[i]))
+
+{
+
+this->firstName = "";
+
+}
+
+else
+
+this->firstName = firstName;
+
+}
+
+for(int i = 0;i < lastName.length();i+=1)
+
+{
+
+if(!isalpha(lastName[i]))
+
+{
+
+this->lastName = "";
+
+}
+
+else
+
+this->lastName = lastName;
+
+}
+
+}
+
+// Copy constructor
+
+Name(const Name& other)
+
+{
+
+firstName = other.firstName;
+
+lastName = other.lastName;
+
+}
+
+// Assignment operator
+
+Name& operator=(const Name& rhs)
+
+{
+
+if(this != &rhs)
+
+{
+
+this->firstName = rhs.firstName;
+
+this->lastName = rhs.lastName;
+
+}
+
+return *this;
+
+}
+
+// Destructor
+
+~Name() {}
+
+std::string& GetFirstName()
+
+{
+
+return firstName;
+
+}
+
+std::string& GetLastName()
+
+{
+
+return lastName;
+
+}
+
+void SetFirstName(const std::string& first)
+
+{
+
+firstName = first;
+
+}
+
+void SetLastName(const std::string& last)
+
+{
+
+lastName = last;
+
+}
+
+std::string ToString() const
+
+{
+
+std::stringstream out;
+
+out << firstName << " " << lastName << "\n";
+
+return out.str();
+
+}
+
+friend std::ostream& operator<<(std::ostream& out,const Name& obj)
+
+{
+
+out << obj.ToString();
+
+return out;
+
+}
+
+};
+
+}
+
+#endif

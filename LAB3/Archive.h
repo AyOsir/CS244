7d6ad6206609fefd4519bf38a4dba37a7d4cb376
Greg56 <32373129+Greg56@users.noreply.github.com>
2017-12-08 20:10:34 -0500

    Update Archive.h

--- LAB3/Archive.h
+++ LAB3/Archive.h
@@ -1,15 +1,49 @@
 #ifndef ARCHIVE_H
 #define ARCHIVE_H
 
-namespace labI
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+namespace lab3
 {
-	void Sort(float[], int);
-	float Maximum(float[], int);
-	float Minimum(float[], int);
-	float Range(float[], int);
-	float Median(float[], int);
-	float Mean(float[], int);
-	float StandardDeviation(float[], int);
-	float OutlinerCount(float[], int);
+	class Name;
+	class Address;
+	class Employee;
+	
+	bool validName(std::string str)
+	{
+		for(int i = 0;i < str.length();i+=1)
+		{
+			if(!isalpha(str[i]))
+				return false;
+		}
+		return true;
+	}
+	
+	bool validInitials(std::string str)
+	{
+		if(str.length() != 2)
+			return false;
+		return (isalpha(str[0]) && isalpha(str[1]));
+	}
+	
+	bool validZipcode(std::string str)
+	{
+		if(str.length() != 5)
+			return false;
+		
+		for(int i = 0;i < 5;i += 1)
+		{
+			if(!isdigit(str[i]))
+				return false;
+		}
+		return true;
+	}
 }
-#endif 
+
+#endif

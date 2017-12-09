19dfb268f26cbb354326de049f51e03a54877023
Greg56 <32373129+Greg56@users.noreply.github.com>
2017-12-08 22:50:57 -0500

    Update Name.h

--- LAB3/Name.h
+++ LAB3/Name.h
@@ -1,172 +1,164 @@
-//Team: Gregory Ayo , Olawale Ayejuyone
-//Author: Gregory Ayo
-//Creation: 12/7/17
+//Team: Gregory Ayo , Olawale Ayejuyone, Barry Boubacar
+//Author: Barry Boubacar
+//Creation: 12/8/17
 #ifndef NAME_H
-
 #define NAME_H
-
 #include "Archive.h"
 
 namespace lab3
 
 {
 
-class Name
-
-{
-
-private:
-
-std::string firstName;
-
-std::string lastName;
-
-public:
-
-Name()
-
-{
-
-firstName = "";
-
-lastName = "";
-
-}
-
-Name(std::string firstName,std::string lastName)
-
-{
-
-for(int i = 0;i < firstName.length();i+=1)
-
-{
-
-if(!isalpha(firstName[i]))
-
-{
-
-this->firstName = "";
-
-}
-
-else
-
-this->firstName = firstName;
-
-}
-
-for(int i = 0;i < lastName.length();i+=1)
-
-{
-
-if(!isalpha(lastName[i]))
-
-{
-
-this->lastName = "";
-
-}
-
-else
-
-this->lastName = lastName;
-
-}
-
-}
-
-// Copy constructor
-
-Name(const Name& other)
-
-{
-
-firstName = other.firstName;
-
-lastName = other.lastName;
-
-}
-
-// Assignment operator
-
-Name& operator=(const Name& rhs)
-
-{
-
-if(this != &rhs)
-
-{
-
-this->firstName = rhs.firstName;
-
-this->lastName = rhs.lastName;
-
-}
-
-return *this;
-
-}
-
-// Destructor
-
-~Name() {}
-
-std::string& GetFirstName()
-
-{
-
-return firstName;
-
-}
-
-std::string& GetLastName()
-
-{
-
-return lastName;
-
-}
-
-void SetFirstName(const std::string& first)
-
-{
-
-firstName = first;
-
-}
-
-void SetLastName(const std::string& last)
-
-{
-
-lastName = last;
-
-}
-
-std::string ToString() const
-
-{
-
-std::stringstream out;
-
-out << firstName << " " << lastName << "\n";
-
-return out.str();
-
-}
-
-friend std::ostream& operator<<(std::ostream& out,const Name& obj)
-
-{
-
-out << obj.ToString();
-
-return out;
-
-}
-
-};
-
-}
-
-#endif
+  class Name
+  
+  {
+  
+    private:
+    
+    std::string firstName;
+    std::string lastName;
+    
+    public:
+    
+    Name()
+    
+    {
+    
+    firstName = "";
+    lastName = "";
+    
+    }
+    
+    Name(std::string firstName,std::string lastName)
+    
+    {
+    
+    for(int i = 0;i < firstName.length();i+=1)
+    
+    {
+    
+    if(!isalpha(firstName[i]))
+    
+    {
+    
+    this->firstName = "";
+    
+    }
+    
+    else
+    
+      this->firstName = firstName;
+    
+    }
+    
+    for(int i = 0;i < lastName.length();i+=1)
+    
+    {
+    
+    if(!isalpha(lastName[i]))
+    
+    {
+    
+      this->lastName = "";
+      
+    }
+    
+    else
+    
+    this->lastName = lastName;
+    
+    }
+    
+    }
+    
+    // Copy constructor
+    
+    Name(const Name& other)
+    
+    {
+      firstName = other.firstName;
+      lastName = other.lastName;
+    }
+    
+    // Assignment operator
+    
+    Name& operator=(const Name& rhs)
+    
+    {
+    
+    if(this != &rhs)
+    
+    {
+    
+      this->firstName = rhs.firstName;
+      this->lastName = rhs.lastName;
+      
+    }
+    
+    return *this;
+    
+    }
+    
+    // Destructor
+    
+    ~Name() {}
+    
+    std::string& GetFirstName()
+    
+    {
+    
+      return firstName;
+    
+    }
+    
+    std::string& GetLastName()
+    
+    {
+    
+      return lastName;
+      
+    }
+    
+    void SetFirstName(const std::string& first)
+    
+    {
+    
+      firstName = first;
+    
+    }
+    
+    void SetLastName(const std::string& last)
+    
+    {
+    
+      lastName = last;
+      
+    }
+    
+    std::string ToString() const
+    
+    {
+    
+      std::stringstream out;
+      
+      out << firstName << " " << lastName << "\n";
+      
+      return out.str();
+    
+    }
+    
+    friend std::ostream& operator<<(std::ostream& out,const Name& obj)
+    
+    {
+    
+      out << obj.ToString();
+      
+      return out;
+    
+    }
+    
+    };
+    
+    }
+  
+  #endif

aef7ff2339c82d28c3e4791ccf58847c86fd72b8
Greg56 <32373129+Greg56@users.noreply.github.com>
2017-12-12 09:28:29 -0500

    Update DiagonalWinner.h

--- LAB2/DiagonalWinner.h
+++ LAB2/DiagonalWinner.h
@@ -9,6 +9,17 @@ namespace lab2
 {
 	bool TicTacToe::diagonalWinner() const
 	{
+		if (board[0][0] == board[1][1] &&
+
+        board[1][1] == board[2][2] &&
+
+        board[0][0] != ' ')
+
+        return(true);
+
+    		if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ')
+
+		return true;
 		return false;
 	}
 }

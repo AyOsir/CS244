d10a1187c6e27ceb3fe318e9730af8fcea1a0255
Greg56 <32373129+Greg56@users.noreply.github.com>
2017-12-12 23:10:45 -0500

    Update VerticalWinner.h

--- LAB2/VerticalWinner.h
+++ LAB2/VerticalWinner.h
@@ -9,7 +9,41 @@ namespace lab2
 {
 	bool TicTacToe::verticalWinner() const
 	{
-		return false;
+		if(board[0,3,6] != '-')
+		{
+		  if(board[0] == board[3]&&board[6]==board[0])
+		  {
+		    return true;
+		  }
+		  else
+		  {
+		    return false;
+		  }
+		}
+		if(board[1,4,7] != '-')
+		{
+		  if(board[1] == board[4]&&board[7]==board[1])
+		  {
+		    return true;
+		  }
+		  else
+		  {
+		    return false;
+		  }
+		}
+		if(board[2,5,8] != '-')
+		{
+		  if(board[2] == board[5]&&board[8]==board[5])
+		  {
+		    return true;
+		  }
+		  else
+		  {
+		    return false;
+		  }
+		}
+		
+	
 	}
 }
 
